4162dc589022ee571d17a19d97a70dab7ca293c6
JuanPatricioBof <juanpatricio@gmail.com>
2025-03-17 20:25:09 -0300

    Tercera modificacion, no uso de indice

--- questions.py
+++ questions.py
@@ -23,14 +23,14 @@ answers = [
  ]
  # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+# Unimos las listas en una sola estructura
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 nota=0
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions)-1)
+for question, options, correct_index in questions_to_ask:
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(question)
+    for i, answer in enumerate(options):
          print(f"{i + 1}. {answer}")
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -44,7 +44,7 @@ for _ in range(3):
             print("Respuesta no valida")
             sys.exit(1)
         # Se verifica si la respuesta es correcta
-        if user_answer ==correct_answers_index[question_index]:
+        if user_answer ==correct_index:
             nota+=1
             print("¡Correcto!")
             break
@@ -55,7 +55,7 @@ for _ in range(3):
         # se muestra la respuesta correcta
         if intento ==1: 
             print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index][correct_answers_index[question_index]])
+            print(options[correct_index])
     # Se imprime un blanco al final de la pregunta
     print()
 print("la nota es:", nota)
